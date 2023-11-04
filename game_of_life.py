12a3ea459e377c839528c75a4f43e94bd76de5ae
hwikle <henry.wikle@gmail.com>
2023-11-04 16:17:56 -0600

    fix update rule

--- game_of_life.py
+++ game_of_life.py
@@ -1,8 +1,12 @@
 import numpy as np
 from scipy import sparse
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
 
 NEIGHBOR_STENCIL = np.ones((3,3), dtype=np.uint64)
 NEIGHBOR_STENCIL[1,1] = 0
+# UPDATE_FUNC = np.vectorize(lambda n: 1 if (n > 1 and n < 4) else 0)
 
 def make_poisson_mtx(stencil: np.ndarray, grid_shape: tuple[int, int]) -> np.ndarray:
     # assume a 3x3 stencil and a square grid
@@ -59,8 +63,46 @@ def make_poisson_mtx(stencil: np.ndarray, grid_shape: tuple[int, int]) -> np.nda
     return sparse.dia_matrix((data, diags),
                              shape=(num_cells, num_cells), dtype=np.uint8)
 
+
+def make_glider() -> np.ndarray:
+    return np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]], dtype=np.uint8)
+
+
+def embed(arr: np.ndarray, grid_width: int) -> np.ndarray:
+    grid = np.zeros((grid_width, grid_width), dtype=np.uint8)
+
+    for i in range(arr.shape[0]):
+        for j in range(arr.shape[1]):
+            grid[i,j] = arr[i,j]
+
+    return grid.flatten()
+
+
+def run(init_state: np.ndarray, ax, max_iter: int = 10) -> np.ndarray:
+    grid_width = int(np.sqrt(init_state.size))
+    grid = init_state
+    mtx = make_poisson_mtx(NEIGHBOR_STENCIL, (grid_width, grid_width))
+
+    ims = []
+    im = ax.imshow(grid.reshape((grid_width, grid_width)), cmap='gray_r', animated=True)
+    ims.append([im])
+
+    for i in range(max_iter):
+        neighbors = mtx @ grid
+        grid = (neighbors == 3) | (grid & (neighbors == 2))
+        im = ax.imshow(grid.reshape((grid_width, grid_width)), cmap='gray_r', animated=True)
+        ims.append([im])
+
+    return ims
+
+
 if __name__ == '__main__':
-    mtx = make_poisson_mtx(NEIGHBOR_STENCIL, (3, 3))
-    print(mtx.toarray())
-    print(mtx.dtype)
-    print((mtx @ NEIGHBOR_STENCIL.flatten()).reshape(3, 3))
+    # grid = np.random.choice([True] + [False] * 249, grid_width**2)
+    grid = embed(make_glider(), 50)
+    fig, ax = plt.subplots()
+    result = run(grid, ax, max_iter=20)
+
+    ani = animation.ArtistAnimation(fig, result, interval=500, blit=True, repeat_delay=1000)
+    ani.save('gol.mp4')
+    # plt.show()
+
