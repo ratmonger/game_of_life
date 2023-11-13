883dbfad87eb9233b98bafc2c1ca274805c7ae1f
hwikle <henry.wikle@gmail.com>
2023-11-13 10:07:31 -0700

    fix bugs in sparse implementation

--- c/sparse.h
+++ c/sparse.h
@@ -3,49 +3,49 @@ struct COOBooleanMatrix {
     unsigned* cols;
     unsigned num_nonzero;
     unsigned capacity;
-}
+};
 
 struct SparseBooleanVector {
     unsigned* indices;
     unsigned num_nonzero;
     unsigned capacity;
-}
+};
 
 struct SparseCharVector {
     unsigned* indices;
     char* values;
     unsigned num_nonzero;
     unsigned capacity;
-}
+};
 
-COOBooleanMatrix* init_COO_boolmat(unsigned init_capacity);
+struct COOBooleanMatrix* init_COO_boolmat(unsigned init_capacity);
 
-SparseBooleanVector* init_boolvec(unsigned init_capacity);
+struct SparseBooleanVector* init_boolvec(unsigned init_capacity);
 
-SparseCharVector* init_charvec(unsigned init_capacity);
+struct SparseCharVector* init_charvec(unsigned init_capacity);
 
-int find_nonzero_boomat(COOBooleanMatrix* mtx, unsigned row, unsigned col);
+int find_nonzero_boomat(struct COOBooleanMatrix* mtx, unsigned row, unsigned col);
 
-int find_nonzero_boolvec(SparseBooleanVector* vec, unsigned idx);
+int find_nonzero_boolvec(struct SparseBooleanVector* vec, unsigned idx);
 
-int find_nonzero_charvec(SparseCharVector* vec, unsigned idx);
+int find_nonzero_charvec(struct SparseCharVector* vec, unsigned idx);
 
 void copy_array(unsigned* a, unsigned* b, unsigned n);
 
-void double_size_boolmat(COOBooleanMatrix* mtx);
+void double_size_boolmat(struct COOBooleanMatrix* mtx);
 
-void double_size_boolvec(SparseBooleanVector* vec);
+void double_size_boolvec(struct SparseBooleanVector* vec);
 
-void double_size_charvec(SparseCharVector* vec);
+void double_size_charvec(struct SparseCharVector* vec);
 
-void add_nonzero_boolmat(COOBooleanMatrix* mtx, unsigned row, unsigned col);
+void add_nonzero_boolmat(struct COOBooleanMatrix* mtx, unsigned row, unsigned col);
 
-void add_nonzero_boolvec(SparseBooleanVector* vec, unsigned idx);
+void add_nonzero_boolvec(struct SparseBooleanVector* vec, unsigned idx);
 
-void add_nonzero_charvec(SparseCharVector* vec, unsigned idx, char val);
+void add_nonzero_charvec(struct SparseCharVector* vec, unsigned idx, char val);
 
-void remove_nonzero_boolmat(COOBooleanMatrix* mtx, unsigned row, unsigned col);
+void remove_nonzero_boolmat(struct COOBooleanMatrix* mtx, unsigned row, unsigned col);
 
-void remove_nonzero_boolvec(SparseBooleanVector* vec, unsigned idx);
+void remove_nonzero_boolvec(struct SparseBooleanVector* vec, unsigned idx);
 
-void remove_nonzero_charvec(SparseCharVector* vec, unsigned idx);
+void remove_nonzero_charvec(struct SparseCharVector* vec, unsigned idx);
--- c/test_sparse.c
+++ c/test_sparse.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "sparse.h"
 
-void main() {
+int main() {
     printf("Hello, world!");
 }
