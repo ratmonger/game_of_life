381829e2a4516f956ddb8232c5af71ee6494587d
hwikle <henry.wikle@gmail.com>
2023-11-30 14:59:56 -0700

    add function descriptions to parallel.h and communication.h

--- c/communication.h
+++ c/communication.h
@@ -1,19 +1,54 @@
 #include "parallel.h"
 
+
+/* Perform a single round of communication (sending and receiving) with the lefthand neighbor,
+ * and store the received neighbors in grid->left. If no lefthand neighbor exists,
+ * does nothing and causes no errors. Assumes a square number of processes. */
 void communicate_left(struct AugmentedDomain* grid, unsigned long grid_width, struct DomainEdges* edges, unsigned rank, unsigned num_procs);
 
+
+/* Perform a single round of communication (sending and receiving) with the righthand neighbor,
+ * and store the received neighbors in grid->right. If no righthand neighbor exists,
+ * does nothing and causes no errors. Assumes a square number of processes. */
 void communicate_right(struct AugmentedDomain* grid, unsigned long grid_width, struct DomainEdges* edges, unsigned rank, unsigned num_procs);
 
+
+/* Perform a single round of communication (sending and receiving) with the above neighbor,
+ * and store the received neighbors in grid->top. If no above neighbor exists,
+ * does nothing and causes no errors. Assumes a square number of processes. */
 void communicate_above(struct AugmentedDomain* grid, unsigned long grid_width, struct DomainEdges* edges, unsigned rank, unsigned num_procs);
 
+
+/* Perform a single round of communication (sending and receiving) with the below neighbor,
+ * and store the received neighbors in grid->bottom. If no below neighbor exists,
+ * does nothing and causes no errors. Assumes a square number of processes. */
 void communicate_below(struct AugmentedDomain* grid, unsigned long grid_width, struct DomainEdges* edges, unsigned rank, unsigned num_procs);
 
+
+/* Perform a single round of communication (sending and receiving) with the upper left
+ * diagonal neighbor, and store the received neighbor in grid->topL. If no upper left neighbor
+ * exists, does nothing and causes no errors. Assumes a square number of processes. */
 void communicate_upper_left(struct AugmentedDomain* grid, unsigned long grid_width, struct DomainEdges* edges, unsigned rank, unsigned num_procs);
 
+
+/* Perform a single round of communication (sending and receiving) with the upper right
+ * diagonal neighbor, and store the received neighbor in grid->topR. If no upper right neighbor
+ * exists, does nothing and causes no errors. Assumes a square number of processes. */
 void communicate_upper_right(struct AugmentedDomain* grid, unsigned long grid_width, struct DomainEdges* edges, unsigned rank, unsigned num_procs);
 
+
+/* Perform a single round of communication (sending and receiving) with the lower right
+ * diagonal neighbor, and store the received neighbor in grid->botR. If no lower right neighbor
+ * exists, does nothing and causes no errors. Assumes a square number of processes. */
 void communicate_lower_right(struct AugmentedDomain* grid, unsigned long grid_width, struct DomainEdges* edges, unsigned rank, unsigned num_procs);
 
+
+/* Perform a single round of communication (sending and receiving) with the lower left
+ * diagonal neighbor, and store the received neighbor in grid->botL. If no lower left neighbor
+ * exists, does nothing and causes no errors. Assumes a square number of processes. */
 void communicate_lower_left(struct AugmentedDomain* grid, unsigned long grid_width, struct DomainEdges* edges, unsigned rank, unsigned num_procs);
 
+
+/* Perform a single round of communication (sending and receiving) with all neighbors, and store
+ * the received neighbors on the provided AugmentedDomain object. Assumes a square number of processes. */
 void communicate_edges(struct AugmentedDomain* grid, unsigned long grid_width, struct DomainEdges* edges, unsigned rank, unsigned num_procs);
--- c/parallel.h
+++ c/parallel.h
@@ -1,8 +1,12 @@
 #ifndef PARALLEL_H
 #define PARALLEL_H
 
+
 /* n x n local domain augmented by the edges and corners of domains on
- * neighboring processes. */
+ * neighboring processes. interior (length n*n) represents the local grid, whereas
+ * the other four char arrays represent edge data on other processes. These four
+ * arrays, as well as the four single chars, are intended only for incoming
+ * communication of data. */
 struct AugmentedDomain{
     unsigned char *interior;
     unsigned char *top;
@@ -15,6 +19,10 @@ struct AugmentedDomain{
     unsigned char botR;
 };
 
+
+/* A heterogeneous collection of the edges and corners of a local n x n grid.
+ * Intended only for outgoing communication. For an n x n grid, the four char
+ * arrays are each length n. */
 struct DomainEdges{
   unsigned char *top;
   unsigned char *bottom;
@@ -26,24 +34,52 @@ struct DomainEdges{
   unsigned char botR;
 };
 
-struct AugmentedDomain* init_domain(unsigned long n);
 
+/* Creates and initializes an AugmentedDomain to the approprate size. Assumes an n x n local domain.
+ * Initialized values are guaranteed to be zeros. */
+struct AugmentedDomain* init_domain(unsigned long n);
+ 
+
+/* Creates an initializes a DomainEdges object to the appropriate size, assuming an n x n local domain.
+ * Initialized values are not guaranteed to be zeros. */
 struct DomainEdges* init_edges(unsigned long n);
 
-void get_edges(unsigned char* grid, struct DomainEdges* edges, unsigned long n);
 
-struct AugmentedDomain* partitions(unsigned char* grid, unsigned long n);
-
-unsigned char count_neighbors_parallel(struct AugmentedDomain* partition, unsigned long n, unsigned long i);
-
-void update_state_parallel(struct AugmentedDomain* grid, unsigned long n);
-
+/* Deallocates an AugmentedDomain object */
 void free_augmented_domain(struct AugmentedDomain* domain);
 
+
+/* Deallocates a DomainEdges object */
 void free_domain_edges(struct DomainEdges* edges);
 
+
+/* Extracts the edges from the n x n grid and places them into the provided edges object .*/
+void get_edges(unsigned char* grid, struct DomainEdges* edges, unsigned long n);
+
+
+/* Takes a grid of size (n x 2) x (n x 2), representing a local grid and its neighboring edges,
+ * and separates it into the local portion and its edges, all of which are placed into the
+ * appropriate fields on the AugmentedDomain object. */
+struct AugmentedDomain* partitions(unsigned char* grid, unsigned long n);
+
+
+/* Counts the neighbors of the cell at the index i, taking into account the non-local edges stored in the
+ * AugmentedDomain object. Assumes edges have already been communicated and placed into the appropriate field. */
+unsigned char count_neighbors_parallel(struct AugmentedDomain* partition, unsigned long n, unsigned long i);
+
+
+/* Updates the state of AugmentedGrid interior according to the rules of the Game of Life. Assumes edges have
+ * already been communicated. Does not modify edges, only the interior cells. */
+void update_state_parallel(struct AugmentedDomain* grid, unsigned long n);
+
+
+/* Returns TRUE if all values along the edge of a domain (represented in the DomainEdges object) are zero,
+ * or FALSE otherwise. Intended for debugging. */
 unsigned char is_empty(struct DomainEdges* edges, unsigned long n);
 
+
+/* Counts the number of exterior neighbors (e.g. neighboring cells that exist on other processes). Assumes
+ * edges have already been communicated. Intended for debugging. */
 unsigned char count_exterior(struct AugmentedDomain* grid, unsigned long n);
 
 #endif
