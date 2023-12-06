925b177688670af716fd54208982ad47b1aff9ac
ratmonger <bombadilmaster@gmail.com>
2023-12-06 16:57:52 -0700

    test64 hopper

--- slurm/time-test64.sh
+++ slurm/time-test64.sh
@@ -6,8 +6,7 @@
 #SBATCH --nodes=2
 #SBATCH --time 20:00:00
 #SBATCH --job-name L64
-#SBATCH --output outputs/naive-life1000ticks-64procs-hopv2.out
-=======
+#SBATCH --output outputs/naive-life1000ticks-64procs-hopv2.out=
 #SBATCH --ntasks-per-node=8
 #SBATCH --nodes=8
 #SBATCH --time 20:00:00
