91e8fc5091e3f12c80b9d87331bbcc071ac47375
frog-overlord <victorialien94@gmail.com>
2023-12-05 22:11:31 -0700

    add slirm scritps

--- slurm/time-test.sh
+++ slurm/time-test.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 #SBATCH --partition normal
-#SBATCH --ntasks-per-node=8
-#SBATCH --nodes=8
-#SBATCH --time 00:30:00
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --time 10:00:00
 #SBATCH --job-name naive-life
-#SBATCH --output naive-life.out
+#SBATCH --output outputs/serial1000ticks.out
 #SBATCH --mail-user dunharrow@unm.edu
 
 module load openmpi
 module load gcc
 
 cd cpp
-srun -n 64 ./output 131072 1000
+srun ./serial.out 64 1000
+srun ./serial.out 128 1000
+srun ./serial.out 256 1000
+srun ./serial.out 512 1000
+srun ./serial.out 1024 1000
+srun ./serial.out 2048 1000
+srun ./serial.out 4096 1000
+srun ./serial.out 8192 1000
+srun ./serial.out 16384 1000
+srun ./serial.out 32768 1000
+srun ./serial.out 65536 1000
+srun ./serial.out 131072 1000
---
+++ slurm/time-test4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --partition normal
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=4
+#SBATCH --nodes=1
+#SBATCH --time 10:00:00
+#SBATCH --job-name naive-life
+#SBATCH --output outputs/naive-life1000ticks-4procs.out
+#SBATCH --mail-user dunharrow@unm.edu
+
+module load openmpi
+module load gcc
+
+cd cpp
+srun -n 4 ./output 64 1000
+srun -n 4 ./output 128 1000
+srun -n 4 ./output 256 1000
+srun -n 4 ./output 512 1000
+srun -n 4 ./output 1024 1000
+srun -n 4 ./output 2048 1000
+srun -n 4 ./output 4096 1000
+srun -n 4 ./output 8192 1000
+srun -n 4 ./output 16384 1000
+srun -n 4 ./output 32768 1000
+srun -n 4 ./output 65536 1000
+srun -n 4 ./output 131072 1000
