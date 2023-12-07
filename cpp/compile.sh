b3d465036cd20828ddbfee437a82ce54e48149a5
Bryce Palmer <dunharrow@unm.edu>
2023-12-07 06:55:28 +0000

    Update compile.sh

--- cpp/compile.sh
+++ cpp/compile.sh
@@ -1,4 +1,4 @@
-mpicxx -o output time_life.cpp mpi_life.cpp naive.cpp openmp.cpp concurrency.cpp concurrency_openmp.cpp
+mpicxx -o output time_life.cpp mpi_life.cpp naive.cpp openmp.cpp concurrency.cpp concurrency_openmp.cpp concurrencyTestAll.cpp
 
 mpicxx -o serial.out serial.cpp
 
